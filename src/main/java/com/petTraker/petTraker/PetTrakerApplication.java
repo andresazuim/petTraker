09ac76e80c38aa89f1c26687333f810da0cec7f9
andresazuim <andresazuim37@gmail.com>
2024-09-28 23:09:41 -0300

    Primeiro commit

---
+++ src/main/java/com/petTraker/petTraker/PetTrakerApplication.java
@@ -0,0 +1,13 @@
+package com.petTraker.petTraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTrakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petTraker/petTraker/PetTrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petTraker.petTraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
